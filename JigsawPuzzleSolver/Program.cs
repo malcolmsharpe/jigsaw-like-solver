5f08e545c0b26764c55df9f0861a46377ab9a91a
malcolmsharpe <sharpe.malcolm@gmail.com>
2014-12-27 18:48:47 -0800

    Find all forms of solution.

--- JigsawPuzzleSolver/Program.cs
+++ JigsawPuzzleSolver/Program.cs
@@ -9,9 +9,15 @@ namespace JigsawPuzzleSolver
 {
     class Program
     {
-        const int P = 16;
+        const int P = 16; // number of pieces
+        const int T = 4; // number of connectors
         const int R = 4, C = 4;
         const int DIRS = 4;
+
+        const int LEFT = 0;
+        const int UP = 1;
+        const int RIGHT = 2;
+        const int DOWN = 3;
 
         // W, N, E, S
         //
@@ -19,67 +25,52 @@ namespace JigsawPuzzleSolver
         // 1: cross
         // 2: out arrow (big)
         // 3: in arrow (small)
+        //
+        // "T+" indicates indent. Otherwise, it's outdent.
         int[,] pieces =
             new int[P, DIRS] {
-                { 0, 2, 2, 0 }, // 1
-                { 0, 3, 3, 2 }, // 2
-                { 1, 2, 1, 3 }, // 3
-                { 3, 0, 1, 0 }, // 4
+                { 0, 2, T+2, T+0 }, // 1
+                { 0, 3, T+3, T+2 }, // 2
+                { 1, 2, T+1, T+3 }, // 3
+                { 3, 0, T+1, T+0 }, // 4
 
-                { 2, 0, 0, 1 }, // 5
-                { 3, 2, 2, 0 }, // 6
-                { 3, 3, 1, 0 }, // 7
-                { 0, 3, 1, 3 }, // 8
+                { 2, 0, T+0, T+1 }, // 5
+                { 3, 2, T+2, T+0 }, // 6
+                { 3, 3, T+1, T+0 }, // 7
+                { 0, 3, T+1, T+3 }, // 8
 
-                { 3, 3, 2, 1 }, // 9
-                { 2, 2, 0, 3 }, // 10
-                { 0, 1, 2, 2 }, // 11
-                { 0, 0, 0, 2 }, // 12
+                { 3, 3, T+2, T+1 }, // 9
+                { 2, 2, T+0, T+3 }, // 10
+                { 0, 1, T+2, T+2 }, // 11
+                { 0, 0, T+0, T+2 }, // 12
 
-                { 1, 2, 2, 0 }, // 13
-                { 1, 3, 3, 2 }, // 14
-                { 1, 0, 0, 3 }, // 15
-                { 1, 0, 0, 1 }, // 16
+                { 1, 2, T+2, T+0 }, // 13
+                { 1, 3, T+3, T+2 }, // 14
+                { 1, 0, T+0, T+3 }, // 15
+                { 1, 0, T+0, T+1 }, // 16
             };
         bool[] used = new bool[P];
 
-        // original:  1,  2, ...,  16
-        // flipped:  -1, -2, ..., -16
-        int[,] grid = new int[R, C];
+        // Flip: reverse connector array _before_ rotation. (Same as mirroring along SW-NE line.)
+        // Rot: Number of steps to rotate counter-clockwise.
+        int[,] gridPiece = new int[R, C];
+        bool[,] gridFlip = new bool[R, C];
+        int[,] gridRot = new int[R, C];
 
         int numSolutions = 0;
 
-        private bool connects(int dir, int p1, int p2)
+        // Get the specified connector of the flipped, rotated piece at the grid point.
+        private int getConnector(int r, int c, int dir)
         {
-            Debug.Assert(2 <= dir && dir < DIRS);
-            Debug.Assert(-16 <= p1 && p1 <= -1 || 1 <= p1 && p1 <= 16);
-            Debug.Assert(-16 <= p2 && p2 <= -1 || 1 <= p2 && p2 <= 16);
+            dir = (dir + gridRot[r, c]) % DIRS;
+            if (gridFlip[r, c]) dir = 3 - dir;
 
-            int d1 = dir;
-            if (p1 < 0)
-            {
-                p1 *= -1;
-                if (d1 == 2) d1 = 3;
-                else
-                {
-                    Debug.Assert(d1 == 3);
-                    d1 = 2;
-                }
-            }
+            return pieces[gridPiece[r, c], dir];
+        }
 
-            int d2 = dir - 2;
-            if (p2 < 0)
-            {
-                p2 *= -1;
-                if (d2 == 0) d2 = 1;
-                else
-                {
-                    Debug.Assert(d2 == 1);
-                    d2 = 0;
-                }
-            }
-
-            return pieces[p1 - 1, d1] == pieces[p2 - 1, d2];
+        private bool connects(int t1, int t2)
+        {
+            return (t1 + T) % (2 * T) == t2;
         }
 
         private void rec(int r, int c)
@@ -90,7 +81,7 @@ namespace JigsawPuzzleSolver
             }
 
             if (c == C) {
-                ++numSolutions;
+                incSolutions();
                 printgrid();
                 return;
             }
@@ -100,19 +91,36 @@ namespace JigsawPuzzleSolver
                 if (used[p]) continue;
                 used[p] = true;
 
-                for (int flip = -1; flip <= 1; flip += 2)
+                gridPiece[r, c] = p;
+                for (int flip = 0; flip <= 1; ++flip)
                 {
-                    grid[r, c] = flip * (p + 1);
+                    gridFlip[r, c] = flip == 1;
 
-                    if (r > 0 && !connects(3, grid[r - 1, c], grid[r, c])) continue;
-                    if (c > 0 && !connects(2, grid[r, c - 1], grid[r, c])) continue;
+                    for (int rot = 0; rot < DIRS; ++rot)
+                    {
+                        gridRot[r, c] = rot;
 
-                    rec(r + 1, c);
+                        // TODO: Break symmetry here by requiring particular flip/rot of piece 0.
+
+                        if (r > 0 && !connects(getConnector(r - 1, c, DOWN), getConnector(r, c, UP))) continue;
+                        if (c > 0 && !connects(getConnector(r, c - 1, RIGHT), getConnector(r, c, LEFT))) continue;
+
+                        rec(r + 1, c);
+                    }
                 }
 
                 used[p] = false;
             }
-            grid[r, c] = 0;
+        }
+
+        private void incSolutions()
+        {
+            ++numSolutions;
+
+            if (numSolutions % 1000 == 0)
+            {
+                Console.WriteLine("Found solution {0}...", numSolutions);
+            }
         }
 
         private void printgrid()
@@ -121,17 +129,17 @@ namespace JigsawPuzzleSolver
             {
                 for (int c = 0; c < C; ++c)
                 {
-                    Console.Write(grid[r, c] + " ");
+                    Console.Write(gridPiece[r, c] + ", " + gridFlip[r,c] + ", " + gridRot[r,c] + "   ");
                 }
                 Console.WriteLine();
             }
+
             Console.WriteLine();
+            Console.ReadLine();
         }
 
         public void run()
         {
-            // TODO: This solver is currently restricted to solutions where the left and top edges
-            // are fully positive and the right and bottom edges are fully negative.
             rec(0, 0);
 
             Console.WriteLine("Total solutions found:  " + numSolutions);
